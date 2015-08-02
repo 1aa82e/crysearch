5126c1a940292298e6946da3d8a9f9face4240a8
evolution536 <gijsrijnders@live.nl>
2015-08-02 10:19:04 +0200

    Initiated v2.0, a new release with major improvements and dropped XP support

--- resource.h
+++ resource.h
@@ -9,9 +9,9 @@
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
  
-#define VERSION_MAJOR               1
+#define VERSION_MAJOR               2
 #define VERSION_MINOR               0
-#define VERSION_REVISION            18
+#define VERSION_REVISION            0
 #define VERSION_BUILD               0
  
 #define VER_FILE_DESCRIPTION_STR    "CrySearch Memory Scanner, Debugger and Disassembler"
